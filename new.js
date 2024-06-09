8c843401e2d245d7d7b8746aaa7c7db33173a598
Erum Sikander <mrsikanderghouri@gmail.com>
2024-06-09 13:27:07 +0500

    done repo practice

---
+++ new.js
@@ -0,0 +1,7 @@
+"use strict";
+// Sir-Zia-repo-04   practice:
+// Assignability_error define
+Object.defineProperty(exports, "__esModule", { value: true });
+let message = "Erum Sikander";
+// message = 10 ;
+console.log(message);
---
+++ new.ts
@@ -0,0 +1,12 @@
+
+
+// Sir-Zia-repo-04   practice:
+// Assignability_error define
+
+let message : string = "Erum Sikander";
+
+// message = 10 ;
+
+console.log(message);
+
+
